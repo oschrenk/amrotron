c9501d63a16e327f18fe9d2e33a800f747103e02
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-05-05 20:42:13 +0200

    Rename TransactionSpec to DetailsSpec

--- src/test/scala/model/DetailsSpec.scala
+++ src/test/scala/model/DetailsSpec.scala
@@ -2,7 +2,7 @@ package model
 
 import org.scalatest._
 
-class TransactionSpec extends FlatSpec with Matchers {
+class DetailsSpec extends FlatSpec with Matchers {
 
   "Row" should "parse single line" in {
     val row = """123456789	EUR	20170103	24000,50	20000,50	20170103	-4000,00	SEPA Acceptgirobetaling          IBAN: NL12INGB0001234567        BIC: INGBNL2A                    Naam: Belastingsdienst          Betalingskenm.: 2222222222222222"""
@@ -106,4 +106,5 @@ class TransactionSpec extends FlatSpec with Matchers {
       case _ => fail()
     }
   }
+
 }
