faa5f6a03cb742606a4ad43cfeefb29ac7af587d
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-05-08 11:35:03 +0200

    Add TransformerSpec

---
+++ src/test/scala/rules/TransformerSpec.scala
@@ -0,0 +1,20 @@
+package rules
+
+import java.time.LocalDate
+
+import model.{ParsedRow, Sepa}
+import org.scalatest.{FlatSpec, Matchers}
+
+class TransformerSpec extends FlatSpec with Matchers {
+
+  "Transformer" should "apply single rule" in {
+    val sepa = Sepa("category", "NL123", None, "Acme", Some("description"))
+    val row = ParsedRow("123", "EUR", LocalDate.of(2017, 5, 8), 20, 10, 10, sepa, "fakeHash")
+    val predicate = IbanPredicate("NL12")
+    val rule = rules.Rule(List("foo"), predicate)
+    val transfomer = new Transformer(List(rule))
+    val t = transfomer.apply(row)
+    t.tags shouldBe List("foo")
+  }
+
+}
