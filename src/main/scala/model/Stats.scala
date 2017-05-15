abba3215985d01b9ec7ae1e1a8cc02108f07f451
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-05-15 12:08:09 +0200

    Use monoidal map merge

--- src/main/scala/model/Stats.scala
+++ src/main/scala/model/Stats.scala
@@ -1,6 +1,26 @@
 package model
 
+trait Monoid[M] {
+  def zero: M
+  def merge(b: M): M
+}
+
+class MonoidMap[K,V](underlying: Map[K, Seq[V]]) extends Monoid[Map[K,Seq[V]]] {
+  val zero = Map.empty
+  def merge(right: Map[K, Seq[V]]) =
+    right.foldLeft(underlying) { (acc, kv) =>
+      val k = kv._1
+      if (acc.contains(k)) {
+        acc.updated(k, acc(k) ++ kv._2)
+      } else {
+        acc ++ Map(kv)
+      }
+    }
+}
+
 object Stats {
+
+  implicit def augmentMap[K,V](m: Map[K,Seq[V]]): MonoidMap[K,V] = new MonoidMap(m)
   def from(transactions: Seq[Transaction]) = {
 
     val groupedTransactions = transactions
@@ -14,14 +34,7 @@ object Stats {
         }
 
         // merge single transaction map into accumulator map
-        tMap.foldLeft(acc) { (l, kv) =>
-          val k = kv._1
-          if (l.contains(k)) {
-            l.updated(k, l(k) ++ kv._2)
-          } else {
-            l ++ Map(kv)
-          }
-        }
+        acc.merge(tMap)
       }
 
       val stats = groupedTransactions.toSeq.map { case (k,v) =>
